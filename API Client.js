f9182efc91a0a5a051b6a5c86feab1bedcedca8f
jaywolf365 <44144430+jaywolf365@users.noreply.github.com>
2019-01-19 21:30:36 -0800

    Add files via upload

---
+++ API Client.js
@@ -0,0 +1,35 @@
+let info = function (vari, num, str){ //sorts data into types
+	let dataType = {};
+	let data = null;
+	switch (data) {
+	  case data == vari:
+	    dataType.variable = vari; //stores data type
+		console.log(); //logs to console
+		break;
+	  case data == num: 
+	    dataType.number = num;
+		console.log();
+		break;
+	  case data == str:
+		dataType.string = str;
+		console.log();
+		break;
+	}
+}
+
+let store = {};
+
+let popular = function(posts){
+	let mostLikes = null;
+	let mostComments = null;
+	switch (posts) {
+		case posts == 1000: 
+		  let post;
+		  store.post1 = post.info();
+		  
+		  console.log();
+		  break;
+	}
+}
+
+popular();
